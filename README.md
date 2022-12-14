8c00c32f90a0ba87b207e7cbf7b769390486f4f2
TallyWright <47222848+TallyWright@users.noreply.github.com>
2022-12-14 12:02:12 +0000

    map.clean added

--- README.md
+++ README.md
@@ -22,3 +22,23 @@ A wrapper for the LDheatmap function (Shin et al. 2006) where $R^{2}$ is calcula
 `chrom` "all" will plot all chromosomes. Alternatively, individual chromosomes can be specified.
 
 `save.png` If `F` plots will be plotted within R, if `T` they will be saved individually as png files. 
+
+## map.clean
+
+### Description 
+
+A method for improving chromosome positioning for physical anchoring of markers using $R^{2}$. $R^{2}$ is calculated for each mapped marker vs. all other markers. All markers above a defined $R^{2}$ threshold are found and the most common chromosome of the markers in that bin is taken. If this chromosome is different to the one currently in the map, blast hits are used to re-position the marker to the new chromosome, if at least one blast hit is present for that chromosome. If multiple BLAST hits are available for the new chromosome, the BLAST hit closest to whichever marker in the bin with the highest $R^{2}$ to the target marker is taken as the position. If all markers used as guides are found on different chromosomes, the chromosome of the marker with the highest $R^{2}$ to the target marker is used as the guide.   
+
+### Usage 
+
+`map.clean(map, geno, blast, r2.thresh = 0.7)`
+
+### Arguments
+
+`map`  A physical map to be improved, stored in a dataframe with three columns in the following order: marker names, chromosome and position.
+
+`geno` Numeric SNP dataframe with marker names as column headers and genotype names as row names. 
+
+`blast` blast hits for a marker probe, stored in a dataframe with three columns: marker names, chromosome and position. Multiple hits are expected for each marker, typically generated using BLAST+.
+
+`r2.thresh` the cut off for defining the bin of correlated markers to use as a guide. The default is 0.7.   
---
+++ map.clean.R
@@ -0,0 +1,69 @@
+map.clean<-function(map, geno, blast, r2.thresh = 0.7){
+  out<-sapply(map[,1], function(x){
+    
+    #calculate cor and pull chromosome of markers over thresh
+    cor.matrix<-cor(geno[,which(colnames(geno)==x)],
+                    geno[,- which(colnames(geno)==x)], 
+                    use = "pairwise.complete.obs")
+    cor.matrix<-round(cor.matrix ^ 2,6)
+    guides <- colnames(cor.matrix)[cor.matrix[1,] > r2.thresh]
+    
+    #if guides are found...
+    if(length(guides) > 0){
+      #pull chromosomes of guides
+      guides <- sapply(guides, function(y){ 
+        map[,2][which(map[,1] == y)] })
+      
+      #if there are multiple guides but all on different chromosome
+      #we need to take one with the highest R2  
+      if(length(guides)==length(unique(guides))){
+        #find signpost that is most correlated marker
+        sign.post<-colnames(cor.matrix)[which.max(cor.matrix)]
+        #pull the chromosome of that marker has the chosen guide
+        c <- map[,2][which(map[,1] == sign.post)] 
+        
+        # if guides are on multiple chromosomes (overlap present) take the most common 
+      }else{
+        #take most common chromosome in guides
+        c <- names(sort(table(guides),decreasing = T))[1]
+      }
+      
+      #check to see if marker is already on most common chromosome in map
+      #TRUE if different chromosome
+      if(!map[,2][which(map[,1]== x )] == c ){
+        #pull blast hits
+        b.hits <- blast[which(blast[,1]==x),]
+        
+        #check if there is a single hit with new chromosome 
+        if(any(b.hits[,2] %in% c) &
+           sum(b.hits[,2] == c) ==1){
+          b.hits[b.hits[,2] %in% c,]#out
+          
+          #if there is multiple hits on new chromosome...
+        }else if( any(b.hits[,2] %in% c) &
+                  sum(b.hits[,2] == c) > 1 ){
+          #subset hits down to new chromosome
+          b.hits<-b.hits[b.hits[,2] %in% c,]
+          #find most correlated marker and call this signpost
+          sign.post<-colnames(cor.matrix)[cor.matrix[1,] > r2.thresh]
+          #subset sign posts down to those on new chromosome
+          sign.post<- sign.post[which(sapply(sign.post, 
+                                             function(y){map[,2][which(map[,1] == y)] }) == c)]
+          #find sign.post with max cor
+          sign.post<-names(which.max(cor.matrix[,colnames(cor.matrix) %in% sign.post]))
+          #find sign post position
+          sign.post<-map[,3][which(map[,1]== sign.post)]
+          #take position closest to signpost 
+          b.hits[ which.min(abs(b.hits[,3]- sign.post)),]#out
+        }
+      }
+    }
+  }      )
+  
+  #remove null parts
+  out<-out[lengths(out) != 0]
+  #convert to df and clean names
+  out<-do.call(rbind.data.frame, out)
+  names(out)<-c("Marker", "Chrom", "Pos")
+  out
+}
